66f2645614e709816fd4353005f42b263a5cbfd1
indigoparadox <indigo.repo@interfinitydynamics.info>
2022-05-27 18:23:23 -0400

    Doc details for altm function.

--- src/mctymaps.h
+++ src/mctymaps.h
@@ -14,11 +14,17 @@
 /*! \brief Width/height of a city map in tiles. */
 #define MCITY_MAP_WIDTH 128
 
+#define MCITY_MAP_ALT_MAX 0x14
+
 /*! \brief Altitude mask for the bits in a short in the ALTM section. */
 #define MCITY_MAP_ALT_MASK 0x1f
 
 /**
  * \brief Get the altitude of the tile at the given position.
+ * \param altm_buf Pointer to the ALTM chunk from which to retrieve the tile.
+ * \param x LR coordinates of the tile, in tiles (from 0 - ::MCITY_MAP_WIDTH).
+ * \param y UD coordinates of the tile, in tiles (from 0 - ::MCITY_MAP_WIDTH).
+ * \return Altitude of the tile (from 0 - ::MCITY_MAP_ALT_MAX).
  */
 uint8_t mcitymaps_get_altm( uint8_t* altm_buf, uint8_t x, uint8_t y );
 
