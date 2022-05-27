4dcb16ddd717f1533bd00be4a88e134bf38d1bb0
indigoparadox <indigo.repo@interfinitydynamics.info>
2022-05-27 00:53:57 -0400

    Added rough doxygen and info sources.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # mcity2k
 
 Simple C library for decoding/working with SimCity 2000 city files.
+
+Based heavily on the following sources:
+
+ * https://github.com/dfloer/SC2k-docs/blob/master/sc2%20file%20spec.md
+ * http://djm.cc/simcity-2000-info.txt
+
--- src/mctyfile.h
+++ src/mctyfile.h
@@ -4,38 +4,96 @@
 
 #include <stdint.h>
 
+/**
+ * \addtogroup mcity2k_chunks Chunks
+ *
+ * \{
+ */
+
+/*! \file mctyfile.h */
+
+/*! \} */
+
+/*! \brief Returned when there is an error parsing a chunk or file. */
 #define MCITYFILE_ERROR 0
 
+/*! \brief Size of the initial file header in bytes. */
 #define MCITY_FILE_HEADER_SZ 12
-#define MCITY_CHUNK_HEADER_SZ 8
+
+/*! \brief Size of the largest specified chunk when uncompressed. */
 #define MCITY_CHUNK_DATA_SZ_MAX 32768
+
+/**
+ * \addtogroup mcity2k_chunks Chunks
+ *
+ * \{
+ */
+
+/*! \brief Size of the header of each chunk in bytes. */
+#define MCITY_CHUNK_HEADER_SZ 8
+/*! \brief Offset of the chunk size within the chunk header. */
 #define MCITY_CHUNK_SZ_OFFSET 4
 
+/*! \} */
+
+/*! \brief Swap the endianness of a 32-bit unsigned integer. */
 #define mcityfile_endian_swap_32( num ) (((num>>24)&0xff) | ((num<<8)&0xff0000) | ((num>>8)&0xff00) | ((num<<24)&0xff000000))
 
+/**
+ * \addtogroup mcity2k_chunks 
+ * \{
+ */
+
+/*! \brief Given the offset of a chunk header, return the chunk's size. */
 #define mcityfile_chunk_sz( city_buf, offset ) (mcityfile_endian_swap_32( *(uint32_t*)&(city_buf[offset + MCITY_CHUNK_SZ_OFFSET]) ))
 
+/*! \brief Given the offset of a chunk header, return the chunk's ID. */
 #define mcityfile_chunk_id( city_buf, offset, id ) id[0] = city_buf[offset]; id[1] = city_buf[offset + 1]; id[2] = city_buf[offset + 2]; id[3] = city_buf[offset + 3];
 
+/*! \} */
+
 struct MCITY_FILE_HEADER {
+   /*! \brief City file magic number. Should always be "FORM". */
    char magic[4];
    uint32_t file_sz;
    char iff_type[4];
 };
 
+/**
+ * \addtogroup mcity2k_chunks 
+ * \{
+ */
+
+/**
+ * \brief Given an offset of a chunk, find the next chunk after it.
+ */
 uint32_t mcityfile_next_chunk(
    const uint8_t* city_buf, uint32_t city_buf_sz, uint32_t prev_offset );
 
+/**
+ * \brief Given an offset of a chunk, find the next chunk after it with the
+ *        given ID.
+ */
 uint32_t mcityfile_find_chunk(
    const uint8_t* city_buf, uint32_t city_buf_sz, char id[5] );
 
+/**
+ * \brief Copy the uncompressed chunk data from the chunk at the given offset
+ *        into the given buffer.
+ */
 uint32_t mcityfile_chunk_data(
    const uint8_t* city_buf, uint32_t offset,
    uint8_t* buffer, uint32_t buffer_sz );
 
+/**
+ * \brief Decompress the chunk data from the chunk at the given offset into
+ *        the given buffer.
+ */
 uint32_t mcityfile_chunk_data_rle(
    const uint8_t* city_buf, uint32_t offset,
    uint8_t* buffer, uint32_t buffer_sz );
+
+/*! \} */
 
 #endif /* !MCTYFILE_H */
 
